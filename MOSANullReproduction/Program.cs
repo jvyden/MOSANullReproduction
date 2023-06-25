7e63f6f3349c0b2f41c3b981772a51d9ac3fadbf
jvyden <jvyden@jvyden.xyz>
2023-06-25 17:42:13 -0400

    Initial commit

---
+++ MOSANullReproduction/Program.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) MOSA Project. Licensed under the New BSD License.
+
+// #nullable disable
+#nullable enable
+
+using System;
+
+// ReSharper disable all
+
+namespace MOSANullReproduction;
+
+public static class Program
+{
+    public static string String = null!;
+    
+    public static void Setup()
+    {
+        String = "Hello!";
+        Console.WriteLine(String);
+    }
+
+    public static void Loop()
+    {
+    }
+}
